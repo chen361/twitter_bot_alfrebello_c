bc1acc78829db05c6ffb1d3a71219860652c4fb3
Alfie Chen <chen361@outlook.com>
2017-09-12 19:41:41 +0800

    20170912

--- bot.js
+++ bot.js
@@ -1,7 +1,4 @@
-console.log("bot is running");
-
 var Twit = require('twit');
-
 var config = require("./config");
 var T = new Twit(config);
 
@@ -12,42 +9,13 @@ var filter_object = {
     track: hashtags,
     language: 'en, zh_tw'
 }
-//
-//  stream a sample of public statuses
-//
+
 var stream = T.stream('statuses/filter', filter_object);
 var i = 0;
 
 stream.on('tweet', function(tweet) {
 
     i++;
-    console.log(i + ". " + tweet.user.name + " (" + tweet.user.screen_name + "): " + tweet.text);
+    console.log(i + ". [Timestamp: " + tweet.created_at + "]\n" + tweet.user.name + " (" + tweet.user.screen_name + "): \n" + tweet.text + "\n");
 
 });
-
-//
-//  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//
-// T.get('search/tweets', { q: 'kubernetes', count: 30 }, function(err, data, response) {
-
-//     var tweets = data.statuses;
-//     for (var i = 0; i < tweets.length; i++) {
-//         console.log(i + ". " + tweets[i].user.name + " [" + tweets[i].user.screen_name + "](" + tweets[i].user.lang + "):\n" + tweets[i].text + "\n");
-//     }
-
-// });
-// var following_list = [];
-
-// T.get('friends/ids', getFriendIds);
-
-
-// function getFriendIds(err, data, response) {
-
-//     var ids = data.ids;
-//     for (var i = 0; i < ids.length; i++) {
-//         T.get('users/show', { user_id: ids[i] }, function(err, data, response) {
-//             console.log(data.name);
-//         });
-//     }
-//     //console.log(following_list);
-// }
